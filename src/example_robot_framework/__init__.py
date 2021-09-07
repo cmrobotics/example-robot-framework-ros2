62bd1c1ad729c84ea8a8daea8b64238b79d5b3d6
Joao Carlos Espiuca Monteiro <joao.monteiro@cm-robotics.com>
2021-09-07 13:50:19 +0200

    add a utils file to store generic functions

--- src/example_robot_framework/__init__.py
+++ src/example_robot_framework/__init__.py
@@ -1,4 +1,5 @@
 from . import exceptions
+from . import utils
 
 from .dummy import DummyLibrary
 from .exceptions import ContextException
---
+++ src/example_robot_framework/utils.py
@@ -0,0 +1,2 @@
+def signal_handler(signal, frame):
+    raise KeyboardInterrupt
