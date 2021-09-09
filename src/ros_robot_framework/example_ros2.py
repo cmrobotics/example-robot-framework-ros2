171aaaea6cdfcbe6a6b084da3bb2f71ef03840e9
Joao Carlos Espiuca Monteiro <joao.monteiro@cm-robotics.com>
2021-09-09 10:22:27 +0200

    clean up the example_ros2 file

--- src/ros_robot_framework/example_ros2.py
+++ src/ros_robot_framework/example_ros2.py
@@ -9,8 +9,6 @@ from ros_robot_framework import SelfSpinningNode
 class ExampleVelocityNode(SelfSpinningNode):
     def __init__(self):
         super().__init__('robot_framework_node')
-        timer_period = 0.5  # seconds
-        # self.timer = self.create_timer(timer_period, self.cmd_vel_publisher_callback)
         self.__movement_detected = False
         self.__expecting_to_move = False
 
@@ -23,21 +21,23 @@ class ExampleVelocityNode(SelfSpinningNode):
 
 
     def cmd_vel_listener_callback(self, cmd_msg):
-        self.get_logger().info(f'Velocity Command Received: {cmd_msg}')
         print("Velocity received")
         if self.__expecting_to_move is False or cmd_msg.linear.x == 0:
             return
-
         self.__movement_detected = True
 
     def has_moved(self):
         return self.__movement_detected
 
     def setup(self):
+        print("Setting up from within")
         self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.cmd_vel_subscription = self.create_subscription(Twist, 'robot_twist', self.cmd_vel_listener_callback, 10)
         self.start_spinning()
 
     def teardown(self):
+        # Just to be sure, ask the parent class to stop spinning the executor
         self.stop_spinning()
+
+        # Make sure the node is destroyed
         self.destroy_node()
