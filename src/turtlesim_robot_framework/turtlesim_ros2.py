23402cfe69e5d4521af62120091800adddd5d196
Vivian <vivian.li@cm-robotics.com>
2021-09-10 14:58:38 +0200

    added functions to receive feedback from the action

--- src/turtlesim_robot_framework/turtlesim_ros2.py
+++ src/turtlesim_robot_framework/turtlesim_ros2.py
@@ -8,13 +8,18 @@ from std_srvs.srv import Empty
 from turtlesim_robot_framework import SelfSpinningNode
 from turtlesim.action import RotateAbsolute
 
+import threading
+
+
 class TurtlesimExampleNode(SelfSpinningNode):
     def __init__(self):
         super().__init__('robot_framework_turtlesim_node')
         self.__movement_detected = False
         self.__expecting_to_move = False
         self.reset_succeeded = False
-        self.action_sent = False
+        self.action_succeeded = False
+        self.action_result = None
+        self.action_result_condition = threading.Condition()
 
     def publish_velocity(self, vel):
         move_cmd = Twist()
@@ -49,9 +54,39 @@ class TurtlesimExampleNode(SelfSpinningNode):
         goal_orientation.theta = theta
 
         self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(goal_orientation)
-        self.action_sent = True
+        self.action_result = None
+        send_goal_future = self._action_client.send_goal_async(goal_orientation, feedback_callback=self.feedback_callback)
+        send_goal_future.add_done_callback(self.goal_response_callback)
 
+    def goal_response_callback(self, future):
+        print('goal response received')
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+        self.get_logger().info('Goal accepted :)')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        print('goal result received')
+        with self.action_result_condition:
+            self.action_result = future.result().result
+            self.get_logger().info('Result: {0}'.format(self.action_result))
+            self.action_result_condition.notify()
+
+    def feedback_callback(self, feedback_msg):
+        print('goal feedback received')
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+
+    def wait_for_action_result(self, timeout=None):
+        with self.action_result_condition:
+            ret = self.action_result_condition.wait_for(self.result_available, timeout=timeout)
+            return self.action_result if ret is True else None
+
+    def result_available(self):
+        return self.action_result is not None
 
     def setup(self):
         self.cmd_vel_publisher = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
